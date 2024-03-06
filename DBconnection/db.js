efab5abe8057d73999dd4163309a48c912fe8134
Rahul Tile <tilerahul8068@gmail.com>
2024-03-06 19:49:53 +0530

    minor changes in env

--- DBconnection/db.js
+++ DBconnection/db.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const dbConnect = async () => {
+    
+    const url = process.env.MONGODB_URL || "mongodb+srv://tilerahul8068:rahultile@resumateproject.cpofa7t.mongodb.net/"
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(url);
         console.log("Database Connected Successfully...")
     } catch (error) {
         console.log("Database Connection Failed...")
+        console.log(error);
+        
     }
 }
 module.exports = dbConnect;
--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use("/api/v1/section", sectionRoute);
 
 dbConnect();
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`server is running on port ${process.env.PORT}`)
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`);
 })
