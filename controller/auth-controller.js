986f5d87820bf1a5fea808857b22399378ee34e9
Rahul Tile <tilerahul8068@gmail.com>
2024-07-21 00:26:20 +0530

    forgot password controller created

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -2,82 +2,82 @@ const User = require("../models/userModel");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-const home = (req, res) =>{
+const home = (req, res) => {
     try {
         res.status(200).send("<h1>This is the Resumate Home page</h1>");
     } catch (error) {
-        res.status(404).json({msg:"Page not found"});
+        res.status(404).json({ msg: "Page not found" });
     }
 }
-const login = async (req, res) =>{
+const login = async (req, res) => {
     try {
-        const {email, password} = req.body;
-        if(!email || !password){
+        const { email, password } = req.body;
+        if (!email || !password) {
             return res.status(400).json({
-                success : false,
-                message : 'Please fill all fields',
+                success: false,
+                message: 'Please fill all fields',
             })
         }
 
-        const userExist = await User.findOne({email});
-        if(!userExist){
+        const userExist = await User.findOne({ email });
+        if (!userExist) {
             return res.status(404).json({
-                success : false,
-                message : 'User not found, please register first',
+                success: false,
+                message: 'User not found, please register first',
             })
         }
         const isVerify = await bcrypt.compare(password, userExist.password);
-        if(!isVerify){
+        if (!isVerify) {
             return res.status(404).json({
-                success : false,
-                message : 'Invalid Credentials',
+                success: false,
+                message: 'Invalid Credentials',
             })
         }
         const token = jwt.sign({
-            userId : userExist._id,
-            isAdmin : userExist.isAdmin,
+            userId: userExist._id,
+            isAdmin: userExist.isAdmin,
         },
-        process.env.JWT_SECRET_KEY, {
-            expiresIn : '1h'
+            process.env.JWT_SECRET_KEY, {
+            expiresIn: '1h'
         })
         req.user = token;
         return res.cookie('token', token, {
-            expire: new Date(new Date().getTime()+10*1000,{
-                httpOnly : true
+            expire: new Date(new Date().getTime() + 10 * 1000, {
+                httpOnly: true
             })
         }).status(200).json({
-            success : true,
-            message : 'Login Successful',
-            user : userExist,
-            token 
+            success: true,
+            message: 'Login Successful',
+            user: userExist,
+            token
         })
 
     } catch (error) {
-        res.status(404).json({message:"Page not found"});
+        res.status(404).json({ message: "Page not found" });
     }
 }
 
-const register = async (req, res) =>{
+const register = async (req, res) => {
     try {
-        const {firstName, lastName, email, phone, password, cpassword} = req.body;
+        const { firstName, lastName, email, phone, password, cpassword } = req.body;
 
-        if(!firstName || !lastName || !email || !phone || !password || !cpassword){
+        if (!firstName || !lastName || !email || !phone || !password || !cpassword) {
             return res.status(400).json({
-                success : false,
-                message : 'All Fields are mandatory',
+                success: false,
+                message: 'All Fields are mandatory',
             })
         }
-        const userExist = await User.findOne({email});
-        if(userExist){
+        const userExist = await User.findOne({ email });
+        if (userExist) {
             return res.status(400).json({
-                success : false,
-                message : 'Email already exist',
+                success: false,
+                message: 'Email already exist',
             })
         }
-        if(password !== cpassword){
+        if (password !== cpassword) {
             return res.status(400).json({
-                success : false,
-                message : 'password does not match',
+                success: false,
+                message: 'password does not match',
             })
         }
         // const hashPassword = await bcrypt.hash(password, 10);
@@ -85,22 +85,75 @@ const register = async (req, res) =>{
         // const userCreated = await User.create({firstName, lastName, email, phone, password:hashPassword, imgUrl});
 
         return res.status(200).json({
-            success : true,
-            message : 'All Fields are correct !!',
+            success: true,
+            message: 'All Fields are correct !!',
         })
     } catch (error) {
-        res.status(404).json({message:"Page not found"});
+        res.status(404).json({ message: "Page not found" });
     }
 }
 
-const getAllUser = async (req, res) =>{
+const getAllUser = async (req, res) => {
     try {
         const users = await User.find();
-        
+
         res.status(200).json(users);
     } catch (error) {
-        res.status(404).json({msg:"Page not found"});
+        res.status(404).json({ msg: "Page not found" });
     }
 }
 
-module.exports = {home, register, login, getAllUser}; 
+
+const forgotPassword = async (req, res) => {
+  try {
+    const { email, password, confirmPassword } = req.body;
+
+    if (!email || !password || !confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message: 'All Fields are mandatory',
+      });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message: 'Password and Confirm Password did not match !!',
+      });
+    }
+
+    const userExist = await User.findOne({ email });
+
+    if (!userExist) {
+      return res.status(400).json({
+        success: false,
+        message: 'User Not Found !!',
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.findByIdAndUpdate(userExist._id, { password: hashPassword }, { new: true });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: 'Error while updating password !!',
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'Successfully Changed Password !!',
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Server Error',
+      error: error.message,
+    });
+  }
+};
+
+
+module.exports = { home, register, login, getAllUser, forgotPassword }; 
--- router/auth-router.js
+++ router/auth-router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {home, login, register, getAllUser} = require("../controller/auth-controller")
+const {home, login, register, getAllUser, forgotPassword} = require("../controller/auth-controller")
 
 router.route("/").get(home);
 router.route("/login").post(login);
 router.route("/register").post(register);
 router.route("/getUsers").get(getAllUser);
+router.route("/forgotpassword").put(forgotPassword);
 
 module.exports = router;
